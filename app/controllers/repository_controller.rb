fa1867102359da4fda7800978b43af46961113d8
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-06-19 06:33:59 -0400

    built out basic functionality for indexing and creating repositories w/ strong params

--- app/controllers/repository_controller.rb
+++ app/controllers/repository_controller.rb
@@ -1,7 +1,26 @@
 class RepositoryController < ApplicationController
+  protect_from_forgery except: :create
   def index
+    @repositories = Repository.all
+    # render html: index template
+    render json: @repositories
   end
 
   def create
+    @repository = Repository.new(repository_params)
+    @repository.languages = repository_languages
+    @repository.save
+    render json: @repository
   end
+
+  private
+
+  def repository_params
+    params.require(:repository).permit(:name, :url, :languages)
+  end
+
+  ### Depends on if necessary ###
+  # def repository_languages
+  #   Languages.find_or_create_by(name: params[:repository][:languages])
+  # end
 end
