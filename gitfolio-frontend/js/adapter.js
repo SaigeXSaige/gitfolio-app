3dfab95d4b9d9b7793a0cf810dffc57b68f1e00e
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-06-19 18:21:02 -0400

    Adapter class grabs all users and creates a user

--- gitfolio-frontend/js/adapter.js
+++ gitfolio-frontend/js/adapter.js
@@ -1,8 +1,21 @@
-const url = "http://localhost:3000/"
+const url = "http://localhost:3000/users"
 class Adapter {
     static getUsers() {
-        return fetch(url + 'users')
+        return fetch(url)
             .then(resp => resp.json())
             .then(console.log)
+    }
+
+    static createUser(data) {
+        // Default options are marked with *
+        return fetch(url, {
+            body: JSON.stringify(data), // must match 'Content-Type' header
+            headers: {
+            'content-type': 'application/json'
+            },
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        })
+        .then(response => response.json()) // parses response to JSON
+        .then(console.log)
     }
 }
