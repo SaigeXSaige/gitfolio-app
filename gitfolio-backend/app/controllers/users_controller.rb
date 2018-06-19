79c3ac3064dbd05d72646f8487611adb29ab5f3a
travisdock <travisdock@gmail.com>
2018-06-19 17:27:09 -0400

    add refresh to strong params

--- gitfolio-backend/app/controllers/users_controller.rb
+++ gitfolio-backend/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # protect_from_forgery except: [:create]
-  
+
   def index
     @users = User.all
     # render html: index template
@@ -9,16 +9,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.find_or_create_by(user_params)
-    if @user.repositories.length == 0 
+    if @user.repositories.length == 0
       git_repos = @user.find_repos
       @user.assign_repos(git_repos)
-    end 
+    end
     render json: @user.repositories
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username)
+    params.require(:user).permit(:username, :refresh)
   end
 end
