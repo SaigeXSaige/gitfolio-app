785fbad0a6b77671a0a9a22b220cb4e88bd1c363
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-06-19 06:54:33 -0400

    created basic functionality for indexing and creating users w/ strong params

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,21 @@
 class UserController < ApplicationController
+  protect_from_forgery except: :create
+  
   def index
+    @users = User.all
+    # render html: index template
+    render json: @users
   end
 
-  def create 
+  def create
+    @user = User.new(user_params)
+    @user.save
+    render json: @user
+  end
+
+  private
+
+  def User_params
+    params.require(:user).permit(:username)
   end
 end
