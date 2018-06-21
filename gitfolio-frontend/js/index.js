e7870eea3eca8ccb776d3fd34150fb8669795ab5
SaigeXSaige <36903857+SaigeXSaige@users.noreply.github.com>
2018-06-21 15:39:45 -0400

    moved init function to ContentLoaded event listener

--- gitfolio-frontend/js/index.js
+++ gitfolio-frontend/js/index.js
@@ -8,70 +8,69 @@ function init() {
   User.renderUsers()
 }
 
-init()
 
 function clipBoard(e) {
   let codeText = document.querySelector(`#${e.target.id}-code-hidden`);
   codeText.select();
-
+  
   try { //it's good practice to put execCommands in try catch blocks
-    document.execCommand('copy');
-  } catch (err) {
+  document.execCommand('copy');
+} catch (err) {
     window.alert('Oops, unable to copy');
   }
-
+  
 }
 
 document.querySelector("form").addEventListener("submit", (e) => {
   console.log("submit!")
   let inputEl = document.querySelector("#username-input"),
-    username = inputEl.value.trim().toLowerCase(), regex = /\s/, user;
-
+  username = inputEl.value.trim().toLowerCase(), regex = /\s/, user;
+  
   if (!regex.test(RegExp(username)) &&  username.length !== 0) {
-
+    
     document.querySelector("#html-code").innerText = "Loading template..."
     Adapter.createUserAndRepos(username)
-      .then(repos => {
-        if (repos.length > 0) {
-          if (User.findByUsername(username)) {
-            user = User.findByUsername(username)
-          } else {
-            user = new User({"username": username, "id": repos[0].user_id}, store)
-            user.renderSelf()
-          }
-          // Repository.createUserRepos(user.username, true)
-            // .then(repos => {
-              // console.log(repos);
+    .then(repos => {
+      if (repos.length > 0) {
+        if (User.findByUsername(username)) {
+          user = User.findByUsername(username)
+        } else {
+          user = new User({"username": username, "id": repos[0].user_id}, store)
+          user.renderSelf()
+        }
+        // Repository.createUserRepos(user.username, true)
+        // .then(repos => {
+          // console.log(repos);
           //
           repos.map(repo => new Repository(repo, store)) // make the repos from DB into memory repos
           renderTemplateWithPreview(user)
         }
-
+        
       })
-    inputEl.value = ''
-    e.preventDefault()
-  } else {
+      inputEl.value = ''
+      e.preventDefault()
+    } else {
     alert("Spaces are not allowed")
     inputEl.value = ''
     e.preventDefault()
   }
-
-
+  
+  
 })
 
 document.querySelector("#users").addEventListener("click", (e) => {
   if (e.target.className === "user") {
     let username = e.target.textContent
     Adapter.findUserRepos(username)
-      .then(repos => {
-        user = User.findByUsername(username)
-        renderTemplateWithPreview(user)
-      })
+    .then(repos => {
+      user = User.findByUsername(username)
+      renderTemplateWithPreview(user)
+    })
   }
 })
 
 function cleanStore(array, element) {
-    return array.filter(e => e.userId !== element);
+  return array.filter(e => e.userId !== element);
 }
 
 function renderTemplateWithPreview(user) {
@@ -86,17 +85,17 @@ function renderTemplateWithPreview(user) {
 document.querySelector("#refresh").addEventListener("click", (e) => {
   console.log("refresh!")
   let buttonEl = e.target.dataset.username
-    username = buttonEl,
-    user = User.findByUsername(username);
+  username = buttonEl,
+  user = User.findByUsername(username);
   if (buttonEl !== "none") {
     Adapter.createUserAndRepos(username)
-      .then(repos => {
-        user = User.findByUsername(username)
-        store.repositories = this.cleanStore(store.repositories, user.id)
-        repos.map(repo => new Repository(repo, store)) // make the repos from DB into memory repos
-        Repository.renderTemplateStr(user.repositories(), user) // now we can access them by searching our store
+    .then(repos => {
+      user = User.findByUsername(username)
+      store.repositories = this.cleanStore(store.repositories, user.id)
+      repos.map(repo => new Repository(repo, store)) // make the repos from DB into memory repos
+      Repository.renderTemplateStr(user.repositories(), user) // now we can access them by searching our store
       })
-  }
+    }
 })
 
 document.querySelector("#template-styles-h").addEventListener("click", (e) => {
@@ -104,3 +103,5 @@ document.querySelector("#template-styles-h").addEventListener("click", (e) => {
     clipBoard(e)
   }
 })
+
+document.addEventListener('DOMContentLoaded', init)
